f8f341923faafc4295a8a0e34bdf5067bcb6caf8
Timo Grossenbacher <wnstnsmth@yahoo.com>
2017-02-02 21:10:43 +0100

    more precise matching of pms, now using full names, but not 100% perfect

--- src/js/extractor.js
+++ src/js/extractor.js
@@ -1,7 +1,8 @@
 export default ({content, parliamentarians}) => {
   // magic logic
   // console.log(content)
-  const textIndex = content.split(' ').reduce(
+  const tokens = content.split(' ')
+  const textIndex = tokens.reduce(
     (index, word, i) => {
       index[word] = index[word] || []
       index[word].push(i)
@@ -9,16 +10,45 @@ export default ({content, parliamentarians}) => {
     },
     {}
   )
+  console.log(textIndex)
 
   const lastNames = parliamentarians.map(parl => parl.lastName)
+  // const fullNames = parliamentarians.map(parl => parl.name)
+
   const parlIds = parliamentarians.reduce(
     (index, parl) => {
-      index[parl.lastName] = parl.id
+      index[parl.name] = parl.id
       return index
     },
     {}
   )
-  const matches = lastNames.filter(ln => textIndex[ln]).map(ln => ({matches: textIndex[ln], parlId: parlIds[ln]}))
-  // console.log(matches)
-  return matches.map(match => match.parlId)
+
+  const candidates = lastNames.filter(ln => textIndex[ln]).map(ln => ({matches: textIndex[ln], lastName: ln}))
+  // only find candidates where prename is also available
+  let matches = []
+  console.log(candidates)
+  for (const candidate in candidates) {
+    // go through each candidate and its matches in the textIndex
+    let finalMatchesPerCandidate = []
+    candidates[candidate].matches.forEach((idx) => {
+      const fullNameCandidate = tokens[idx - 1] + ' ' + tokens[idx]
+      console.log(fullNameCandidate)
+      // TODO: not only look for exact matches but also where fullNameCandidate contains a real name
+      let finalMatchPerCandidate = {}
+      if (parlIds[fullNameCandidate] !== undefined) {
+        finalMatchPerCandidate.parlId = parlIds[fullNameCandidate]
+        finalMatchPerCandidate.lastNameIdx = idx
+        finalMatchPerCandidate.firstNameIdx = idx - 1
+      }
+      finalMatchesPerCandidate.push(finalMatchPerCandidate)
+    })
+    // combine all these finalMatchesPerCandidate into matches array, but only those that are not undefined
+    matches = matches.concat(finalMatchesPerCandidate)
+  }
+  // reduce to contain only unique pms (for the current use case, anyway)
+  let finalMatches = [...new Set(matches.map(parl => parl.parlId))].filter(val => val !== undefined)
+
+  console.log(finalMatches)
+
+  return finalMatches
 }
