14bcf7f75be28f12a3d5aa5d5304fc949444eb3d
Timo Grossenbacher <wnstnsmth@yahoo.com>
2017-02-03 13:15:23 +0100

    revised text on popup

--- src/js/popup/Info.js
+++ src/js/popup/Info.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Info = ({size, style, className}) => (
   <div style={{minWidth: 400}}>
     <h1>Lobbylayer</h1>
-    <h2>Wie funktionier es?</h2>
-    <p>Wenn sie auf rts.ch oder srf.ch einen Artikel lesen fügt die Erweiterung am Ende des Artikels eine Sektion mit den Interessenbindungen der erwähnten Personen hinzu.</p>
-    <h2>Fehler</h2>
-    <p>Die Personen werden durch die Nach- und Vornamen erkannt. Es kann zu Fehler – Verwechslungen kommen. Prüfen Sie selbst ob es sich tatsächlich um die selbe Person handelt.</p>
-    <h3>Lobbywatch, Plattform für eine transparente Politik</h3>
+    <h2>Wie funktioniert es?</h2>
+    <p>Wenn Sie auf rts.ch oder srf.ch einen Artikel lesen, fügt diese Erweiterung am Ende des Artikels eine Sektion mit den Interessenbindungen der im Artikel erwähnten Personen hinzu.</p>
+    <h2>Mögliche Probleme</h2>
+    <p>Die Personen werden durch Nach- und Vornamen erkannt. Es kann zu Fehler, sprich zum Beispiel Verwechslungen mit gleichnamigen Personen, kommen. Prüfen Sie selbst, ob es sich tatsächlich um die gesuchte Person handelt.</p>
+    <h3>Datenquelle: Lobbywatch, Plattform für eine transparente Politik</h3>
     <p><a href='https://lobbywatch.ch/de/content/projekt' target='_blank'>Mehr über Lobbywatch erfahren</a></p>
   </div>
 )
