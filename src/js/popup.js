dc40df4663d60c57575b8660da0fec53334784d7
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-03 12:44:10 +0100

    readd popup with info

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,5 @@
 import '../css/popup.css'
-import ParliamentarianList from './popup/ParliamentarianList'
+import Info from './popup/Info'
 import React from 'react'
 import { render } from 'react-dom'
 import client from './graphql'
@@ -8,7 +8,7 @@ import { ApolloProvider } from 'react-apollo'
 render(
   (
     <ApolloProvider client={client}>
-      <ParliamentarianList />
+      <Info />
     </ApolloProvider>
   ),
   window.document.getElementById('app-container')
---
+++ src/js/popup/Info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Info = ({size, style, className}) => (
+  <div>
+    <h1>Wie funktionier es?</h1>
+    <p>Wenn sie auf rts.ch oder srf.ch einen Artikel lesen fügt die Erweiterung am Ende des Artikels eine Sektion mit den Interessenbindungen der erwähnten Personen hinzu.</p>
+    <h2>Fehler</h2>
+    <p>Die Personen werden durch die Nach- und Vornammen erkannt. Es kann zu Fehler – Verwechslungen kommen. Prüfen Sie selbst ob es sich tatsächlich um die selbe Person handelt.</p>
+    <h3>Lobbywatch, Plattform für eine transparente Politik</h3>
+    <p><a href='https://lobbywatch.ch/de/content/projekt' target='_blank'>Mehr über Lobbywatch erfahren</a></p>
+
+  </div>
+)
+
+export default Info
