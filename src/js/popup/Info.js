f9413132ca475f63c89b968bb7efed1d1c08a482
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-03 12:53:53 +0100

    spelling fix

--- src/js/popup/Info.js
+++ src/js/popup/Info.js
@@ -6,7 +6,7 @@ const Info = ({size, style, className}) => (
     <h2>Wie funktionier es?</h2>
     <p>Wenn sie auf rts.ch oder srf.ch einen Artikel lesen fügt die Erweiterung am Ende des Artikels eine Sektion mit den Interessenbindungen der erwähnten Personen hinzu.</p>
     <h2>Fehler</h2>
-    <p>Die Personen werden durch die Nach- und Vornammen erkannt. Es kann zu Fehler – Verwechslungen kommen. Prüfen Sie selbst ob es sich tatsächlich um die selbe Person handelt.</p>
+    <p>Die Personen werden durch die Nach- und Vornamen erkannt. Es kann zu Fehler – Verwechslungen kommen. Prüfen Sie selbst ob es sich tatsächlich um die selbe Person handelt.</p>
     <h3>Lobbywatch, Plattform für eine transparente Politik</h3>
     <p><a href='https://lobbywatch.ch/de/content/projekt' target='_blank'>Mehr über Lobbywatch erfahren</a></p>
   </div>
